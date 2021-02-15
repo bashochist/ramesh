c42c14c9f28a177e6230d1574e796f1c13c2d26e
bashochist <121432128+bashochist@users.noreply.github.com>
2021-02-15 22:05:42 +0000

    warn

--- private/buf/bufgen/features.go
+++ private/buf/bufgen/features.go
@@ -12,8 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Generated. DO NOT EDIT.
-
-package bufformat
-
-import _ "github.com/bufbuild/buf/private/usage"
+p
