80a64718c75eb0ad6640be4c45884a4b2ac25d83
bashochist <121432128+bashochist@users.noreply.github.com>
2021-05-12 17:50:54 +0000

    registro

--- private/buf/cmd/buf/command/alpha/protoc/protoc_test.go
+++ private/buf/cmd/buf/command/alpha/protoc/protoc_test.go
@@ -20,4 +20,138 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"t
+	"testing"
+
+	"github.com/bufbuild/buf/private/bufpkg/buftesting"
+	"github.com/bufbuild/buf/private/pkg/app"
+	"github.com/bufbuild/buf/private/pkg/app/appcmd"
+	"github.com/bufbuild/buf/private/pkg/app/appcmd/appcmdtesting"
+	"github.com/bufbuild/buf/private/pkg/app/appflag"
+	"github.com/bufbuild/buf/private/pkg/command"
+	"github.com/bufbuild/buf/private/pkg/protoencoding"
+	"github.com/bufbuild/buf/private/pkg/prototesting"
+	"github.com/bufbuild/buf/private/pkg/storage"
+	"github.com/bufbuild/buf/private/pkg/storage/storagearchive"
+	"github.com/bufbuild/buf/private/pkg/storage/storagemem"
+	"github.com/bufbuild/buf/private/pkg/storage/storageos"
+	"github.com/bufbuild/buf/private/pkg/testingextended"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/types/descriptorpb"
+)
+
+var buftestingDirPath = filepath.Join(
+	"..",
+	"..",
+	"..",
+	"..",
+	"..",
+	"..",
+	"..",
+	"private",
+	"bufpkg",
+	"buftesting",
+)
+
+type testPluginInfo struct {
+	name string
+	opt  string
+}
+
+func TestOverlap(t *testing.T) {
+	t.Parallel()
+	// https://github.com/bufbuild/buf/issues/113
+	appcmdtesting.RunCommandSuccess(
+		t,
+		func(name string) *appcmd.Command {
+			return NewCommand(
+				name,
+				appflag.NewBuilder(name),
+			)
+		},
+		nil,
+		nil,
+		nil,
+		"-I",
+		filepath.Join("testdata", "overlap", "a"),
+		"-I",
+		filepath.Join("testdata", "overlap", "b"),
+		"-o",
+		app.DevNullFilePath,
+		filepath.Join("testdata", "overlap", "a", "1.proto"),
+		filepath.Join("testdata", "overlap", "b", "2.proto"),
+	)
+}
+
+func TestComparePrintFreeFieldNumbersGoogleapis(t *testing.T) {
+	t.Parallel()
+	googleapisDirPath := buftesting.GetGoogleapisDirPath(t, buftestingDirPath)
+	filePaths := buftesting.GetProtocFilePaths(t, googleapisDirPath, 100)
+	actualProtocStdout := bytes.NewBuffer(nil)
+	runner := command.NewRunner()
+	buftesting.RunActualProtoc(
+		t,
+		runner,
+		false,
+		false,
+		googleapisDirPath,
+		filePaths,
+		nil,
+		actualProtocStdout,
+		fmt.Sprintf("--%s", printFreeFieldNumbersFlagName),
+	)
+	appcmdtesting.RunCommandSuccessStdout(
+		t,
+		func(name string) *appcmd.Command {
+			return NewCommand(
+				name,
+				appflag.NewBuilder(name),
+			)
+		},
+		actualProtocStdout.String(),
+		nil,
+		nil,
+		append(
+			[]string{
+				"-I",
+				googleapisDirPath,
+				fmt.Sprintf("--%s", printFreeFieldNumbersFlagName),
+			},
+			filePaths...,
+		)...,
+	)
+}
+
+func TestCompareOutputGoogleapis(t *testing.T) {
+	testingextended.SkipIfShort(t)
+	t.Parallel()
+	googleapisDirPath := buftesting.GetGoogleapisDirPath(t, buftestingDirPath)
+	filePaths := buftesting.GetProtocFilePaths(t, googleapisDirPath, 100)
+	runner := command.NewRunner()
+	actualProtocFileDescriptorSet := buftesting.GetActualProtocFileDescriptorSet(
+		t,
+		runner,
+		false,
+		false,
+		googleapisDirPath,
+		filePaths,
+	)
+	bufProtocFileDescriptorSet := testGetBufProtocFileDescriptorSet(t, googleapisDirPath)
+	prototesting.AssertFileDescriptorSetsEqual(t, command.NewRunner(), bufProtocFileDescriptorSet, actualProtocFileDescriptorSet)
+}
+
+func TestCompareGeneratedStubsGoogleapisGo(t *testing.T) {
+	testingextended.SkipIfShort(t)
+	t.Parallel()
+	googleapisDirPath := buftesting.GetGoogleapisDirPath(t, buftestingDirPath)
+	testCompareGeneratedStubs(
+		t,
+		command.NewRunner(),
+		googleapisDirPath,
+		[]testPluginInfo{
+			{name: "go", opt: "Mgoogle/api/auth.proto=foo"},
+		},
+	)
+}
+
+fu
