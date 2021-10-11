3086cb89e111745eeada2fdf46c3007beba4c602
bashochist <121432128+bashochist@users.noreply.github.com>
2021-10-11 10:49:53 +0000

    tun: signedness bug in tun_get_user()

--- private/buf/cmd/buf/command/generate/generate_unix_test.go
+++ private/buf/cmd/buf/command/generate/generate_unix_test.go
@@ -10,4 +10,107 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under th
+// limitations under the License.
+
+//go:build aix || darwin || dragonfly || freebsd || (js && wasm) || linux || netbsd || openbsd || solaris
+// +build aix darwin dragonfly freebsd js,wasm linux netbsd openbsd solaris
+
+package generate
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestProtoFileRef(t *testing.T) {
+	tempDirPath := t.TempDir()
+	testRunSuccess(
+		t,
+		"--output",
+		tempDirPath,
+		"--template",
+		filepath.Join("testdata", "protofileref", "buf.gen.yaml"),
+		filepath.Join("testdata", "protofileref", "a", "v1", "a.proto"),
+	)
+	_, err := os.Stat(filepath.Join(tempDirPath, "java", "a", "v1", "A.java"))
+	require.NoError(t, err)
+	_, err = os.Stat(filepath.Join(tempDirPath, "java", "a", "v1", "B.java"))
+	require.Contains(t, err.Error(), "no such file or directory")
+}
+
+func TestOutputWithExclude(t *testing.T) {
+	tempDirPath := t.TempDir()
+	testRunSuccess(
+		t,
+		filepath.Join("testdata", "paths"),
+		"--output",
+		tempDirPath,
+		"--template",
+		filepath.Join("testdata", "paths", "buf.gen.yaml"),
+		"--exclude-path",
+		filepath.Join("testdata", "paths", "a", "v1"),
+		"--exclude-path",
+		filepath.Join("testdata", "paths", "a", "v3"),
+	)
+
+	_, err := os.Stat(filepath.Join(tempDirPath, "java", "a", "v2", "A.java"))
+	require.NoError(t, err)
+	_, err = os.Stat(filepath.Join(tempDirPath, "java", "b", "v1", "B.java"))
+	require.NoError(t, err)
+	_, err = os.Stat(filepath.Join(tempDirPath, "java", "a", "v1", "A.java"))
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "no such file or directory")
+	_, err = os.Stat(filepath.Join(tempDirPath, "java", "a", "v3", "A.java"))
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "no such file or directory")
+	_, err = os.Stat(filepath.Join(tempDirPath, "java", "a", "v3", "foo", "Foo.java"))
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "no such file or directory")
+	_, err = os.Stat(filepath.Join(tempDirPath, "java", "a", "v3", "bar", "Bar.java"))
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "no such file or directory")
+}
+
+func TestOutputWithPathWithinExclude(t *testing.T) {
+	tempDirPath := t.TempDir()
+	testRunSuccess(
+		t,
+		"--output",
+		tempDirPath,
+		"--template",
+		filepath.Join("testdata", "paths", "buf.gen.yaml"),
+		"--path",
+		filepath.Join("testdata", "paths", "a", "v1", "a.proto"),
+		"--exclude-path",
+		filepath.Join("testdata", "paths", "a"),
+	)
+
+	_, err := os.Stat(filepath.Join(tempDirPath, "java", "a", "v1", "A.java"))
+	require.NoError(t, err)
+	_, err = os.Stat(filepath.Join(tempDirPath, "java", "a", "v2", "A.java"))
+	require.Contains(t, err.Error(), "no such file or directory")
+}
+
+func TestOutputWithExcludeWithinPath(t *testing.T) {
+	tempDirPath := t.TempDir()
+	testRunSuccess(
+		t,
+		"--output",
+		tempDirPath,
+		"--template",
+		filepath.Join("testdata", "paths", "buf.gen.yaml"),
+		"--exclude-path",
+		filepath.Join("testdata", "paths", "a", "v1", "a.proto"),
+		"--path",
+		filepath.Join("testdata", "paths", "a"),
+		filepath.Join("testdata", "paths"),
+	)
+
+	_, err := os.Stat(filepath.Join(tempDirPath, "java", "a", "v2", "A.java"))
+	require.NoError(t, err)
+	_, err = os.Stat(filepath.Join(tempDirPath, "java", "b", "v1", "B.java"))
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "no such file or directo
