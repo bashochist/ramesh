2fb4ad45f441173797dc85ffcd9931dc80bf1806
bashochist <121432128+bashochist@users.noreply.github.com>
2021-10-16 20:45:51 +0000

    pd

--- private/buf/cmd/buf/command/lsfiles/lsfiles.go
+++ private/buf/cmd/buf/command/lsfiles/lsfiles.go
@@ -114,4 +114,60 @@ func run(
 	if err != nil {
 		return err
 	}
-	ref, err := buffetch.NewRefParser(container.Logger(), buffetch.RefParserWithProtoFileRefAllowed()).GetRef(ctx,
+	ref, err := buffetch.NewRefParser(container.Logger(), buffetch.RefParserWithProtoFileRefAllowed()).GetRef(ctx, input)
+	if err != nil {
+		return err
+	}
+	storageosProvider := bufcli.NewStorageosProvider(flags.DisableSymlinks)
+	runner := command.NewRunner()
+	clientConfig, err := bufcli.NewConnectClientConfig(container)
+	if err != nil {
+		return err
+	}
+	fileLister, err := bufcli.NewWireFileLister(
+		container,
+		storageosProvider,
+		runner,
+		clientConfig,
+	)
+	if err != nil {
+		return err
+	}
+	fileRefs, fileAnnotations, err := fileLister.ListFiles(
+		ctx,
+		container,
+		ref,
+		flags.Config,
+		flags.IncludeImports,
+	)
+	if err != nil {
+		return err
+	}
+	if len(fileAnnotations) > 0 {
+		// stderr since we do output to stdout potentially
+		if err := bufanalysis.PrintFileAnnotations(
+			container.Stderr(),
+			fileAnnotations,
+			flags.ErrorFormat,
+		); err != nil {
+			return err
+		}
+		return bufcli.ErrFileAnnotation
+	}
+	if flags.AsImportPaths {
+		bufmoduleref.SortFileInfos(fileRefs)
+		for _, fileRef := range fileRefs {
+			if _, err := fmt.Fprintln(container.Stdout(), fileRef.Path()); err != nil {
+				return err
+			}
+		}
+	} else {
+		bufmoduleref.SortFileInfosByExternalPath(fileRefs)
+		for _, fileRef := range fileRefs {
+			if _, err := fmt.Fprintln(container.Stdout(), fileRef.ExternalPath()); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
