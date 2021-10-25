2e2ff595559472b5beca8c05c858366e0014b199
bashochist <121432128+bashochist@users.noreply.github.com>
2021-10-25 06:24:15 +0000

    Add test cases

--- private/buf/cmd/protoc-gen-buf-lint/lint_test.go
+++ private/buf/cmd/protoc-gen-buf-lint/lint_test.go
@@ -50,4 +50,117 @@ func TestRunLint1(t *testing.T) {
 		buf/buf.proto:3:1:Files with package "other" must be within a directory "other" relative to root but were in directory "buf".
 		buf/buf.proto:3:1:Package name "other" should be suffixed with a correctly formed version, such as "other.v1".
 		buf/buf.proto:6:9:Field name "oneTwo" should be lower_snake_case, such as "one_two".
-		buf/buf_two.proto:3:1:Files with package "other" must be within a directory "other"
+		buf/buf_two.proto:3:1:Files with package "other" must be within a directory "other" relative to root but were in directory "buf".
+		buf/buf_two.proto:3:1:Package name "other" should be suffixed with a correctly formed version, such as "other.v1".
+		buf/buf_two.proto:6:9:Field name "oneTwo" should be lower_snake_case, such as "one_two".
+		`,
+	)
+}
+
+func TestRunLint2(t *testing.T) {
+	testRunLint(
+		t,
+		filepath.Join("testdata", "fail"),
+		[]string{
+			filepath.Join("testdata", "fail", "buf", "buf.proto"),
+			filepath.Join("testdata", "fail", "buf", "buf_two.proto"),
+		},
+		"",
+		[]string{
+			normalpath.Join("buf", "buf.proto"),
+		},
+		0,
+		`
+		buf/buf.proto:3:1:Files with package "other" must be within a directory "other" relative to root but were in directory "buf".
+		buf/buf.proto:3:1:Package name "other" should be suffixed with a correctly formed version, such as "other.v1".
+		buf/buf.proto:6:9:Field name "oneTwo" should be lower_snake_case, such as "one_two".
+		`,
+	)
+}
+
+func TestRunLint3(t *testing.T) {
+	testRunLint(
+		t,
+		filepath.Join("testdata", "fail"),
+		[]string{
+			filepath.Join("testdata", "fail", "buf", "buf.proto"),
+			filepath.Join("testdata", "fail", "buf", "buf_two.proto"),
+		},
+		`{"input_config":"testdata/fail/something.yaml"}`,
+		[]string{
+			normalpath.Join("buf", "buf.proto"),
+		},
+		0,
+		`
+		buf/buf.proto:3:1:Files with package "other" must be within a directory "other" relative to root but were in directory "buf".
+		`,
+	)
+}
+
+func TestRunLint4(t *testing.T) {
+	testRunLint(
+		t,
+		filepath.Join("testdata", "fail"),
+		[]string{
+			filepath.Join("testdata", "fail", "buf", "buf.proto"),
+			filepath.Join("testdata", "fail", "buf", "buf_two.proto"),
+		},
+		`{"input_config":{"version":"v1","lint":{"use":["PACKAGE_DIRECTORY_MATCH"]}}}`,
+		[]string{
+			normalpath.Join("buf", "buf.proto"),
+		},
+		0,
+		`
+		buf/buf.proto:3:1:Files with package "other" must be within a directory "other" relative to root but were in directory "buf".
+		`,
+	)
+}
+
+func TestRunLint5(t *testing.T) {
+	testRunLint(
+		t,
+		filepath.Join("testdata", "fail"),
+		[]string{
+			filepath.Join("testdata", "fail", "buf", "buf.proto"),
+			filepath.Join("testdata", "fail", "buf", "buf_two.proto"),
+		},
+		`{"input_config":{"version":"v1","lint":{"use":["PACKAGE_DIRECTORY_MATCH"]}}}`,
+		[]string{
+			normalpath.Join("buf", "buf.proto"),
+		},
+		0,
+		`
+		buf/buf.proto:3:1:Files with package "other" must be within a directory "other" relative to root but were in directory "buf".
+		`,
+	)
+}
+
+func TestRunLint6(t *testing.T) {
+	// specifically testing that output is stable
+	testRunLint(
+		t,
+		filepath.Join("testdata", "fail"),
+		[]string{
+			filepath.Join("testdata", "fail", "buf", "buf.proto"),
+			filepath.Join("testdata", "fail", "buf", "buf_two.proto"),
+		},
+		`{"input_config":{"version":"v1","lint":{"use":["PACKAGE_DIRECTORY_MATCH"]}},"error_format":"json"}`,
+		[]string{
+			normalpath.Join("buf", "buf.proto"),
+		},
+		0,
+		`
+		{"path":"buf/buf.proto","start_line":3,"start_column":1,"end_line":3,"end_column":15,"type":"PACKAGE_DIRECTORY_MATCH","message":"Files with package \"other\" must be within a directory \"other\" relative to root but were in directory \"buf\"."}
+		`,
+	)
+}
+
+func TestRunLint7(t *testing.T) {
+	testRunLint(
+		t,
+		filepath.Join("testdata", "fail"),
+		[]string{
+			filepath.Join("testdata", "fail", "buf", "buf.proto"),
+			filepath.Join("testdata", "fail", "buf", "buf_two.proto"),
+		},
+		`{"input_config":{"version":"v1","lint":{"
