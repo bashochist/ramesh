dbec27f92d00e3c59dabcacafd8cd4a7dfa4fae6
bashochist <121432128+bashochist@users.noreply.github.com>
2021-10-25 07:45:04 +0000

    Back to development: 0.6

--- private/buf/cmd/protoc-gen-buf-lint/lint_test.go
+++ private/buf/cmd/protoc-gen-buf-lint/lint_test.go
@@ -163,4 +163,130 @@ func TestRunLint7(t *testing.T) {
 			filepath.Join("testdata", "fail", "buf", "buf.proto"),
 			filepath.Join("testdata", "fail", "buf", "buf_two.proto"),
 		},
-		`{"input_config":{"version":"v1","lint":{"
+		`{"input_config":{"version":"v1","lint":{"use":["PACKAGE_DIRECTORY_MATCH"]}},"error_format":"json"}`,
+		[]string{
+			normalpath.Join("buf", "buf.proto"),
+		},
+		0,
+		`
+		{"path":"buf/buf.proto","start_line":3,"start_column":1,"end_line":3,"end_column":15,"type":"PACKAGE_DIRECTORY_MATCH","message":"Files with package \"other\" must be within a directory \"other\" relative to root but were in directory \"buf\"."}
+		`,
+	)
+}
+
+func testRunLint(
+	t *testing.T,
+	root string,
+	realFilePaths []string,
+	parameter string,
+	fileToGenerate []string,
+	expectedExitCode int,
+	expectedErrorString string,
+) {
+	t.Parallel()
+	runner := command.NewRunner()
+	testRunHandlerFunc(
+		t,
+		appproto.HandlerFunc(
+			func(
+				ctx context.Context,
+				container app.EnvStderrContainer,
+				responseWriter appproto.ResponseBuilder,
+				request *pluginpb.CodeGeneratorRequest,
+			) error {
+				return handle(
+					ctx,
+					container,
+					responseWriter,
+					request,
+				)
+			},
+		),
+		testBuildCodeGeneratorRequest(
+			t,
+			runner,
+			root,
+			realFilePaths,
+			parameter,
+			fileToGenerate,
+		),
+		expectedExitCode,
+		expectedErrorString,
+	)
+}
+
+func testRunHandlerFunc(
+	t *testing.T,
+	handler appproto.Handler,
+	request *pluginpb.CodeGeneratorRequest,
+	expectedExitCode int,
+	expectedErrorString string,
+) {
+	requestData, err := protoencoding.NewWireMarshaler().Marshal(request)
+	require.NoError(t, err)
+	stdin := bytes.NewReader(requestData)
+	stdout := bytes.NewBuffer(nil)
+	stderr := bytes.NewBuffer(nil)
+
+	exitCode := app.GetExitCode(
+		appproto.Run(
+			context.Background(),
+			app.NewContainer(
+				nil,
+				stdin,
+				stdout,
+				stderr,
+			),
+			handler,
+		),
+	)
+
+	require.Equal(t, expectedExitCode, exitCode, stringutil.TrimLines(stderr.String()))
+	if exitCode == 0 {
+		response := &pluginpb.CodeGeneratorResponse{}
+		// we do not need fileDescriptorProtos as there are no extensions
+		unmarshaler := protoencoding.NewWireUnmarshaler(nil)
+		require.NoError(t, unmarshaler.Unmarshal(stdout.Bytes(), response))
+		require.Equal(t, stringutil.TrimLines(expectedErrorString), response.GetError(), stringutil.TrimLines(stderr.String()))
+	}
+}
+
+func testBuildCodeGeneratorRequest(
+	t *testing.T,
+	runner command.Runner,
+	root string,
+	realFilePaths []string,
+	parameter string,
+	fileToGenerate []string,
+) *pluginpb.CodeGeneratorRequest {
+	fileDescriptorSet, err := prototesting.GetProtocFileDescriptorSet(
+		context.Background(),
+		runner,
+		[]string{root},
+		realFilePaths,
+		true,
+		true,
+	)
+	require.NoError(t, err)
+	nonImportRootRelFilePaths := make(map[string]struct{}, len(fileToGenerate))
+	for _, fileToGenerateFilePath := range fileToGenerate {
+		nonImportRootRelFilePaths[fileToGenerateFilePath] = struct{}{}
+	}
+	imageFiles := make([]bufimage.ImageFile, len(fileDescriptorSet.File))
+	for i, fileDescriptorProto := range fileDescriptorSet.File {
+		_, isNotImport := nonImportRootRelFilePaths[fileDescriptorProto.GetName()]
+		imageFile, err := bufimage.NewImageFile(
+			fileDescriptorProto,
+			nil,
+			"",
+			"",
+			!isNotImport,
+			false,
+			nil,
+		)
+		require.NoError(t, err)
+		imageFiles[i] = imageFile
+	}
+	image, err := bufimage.NewImage(imageFiles)
+	require.NoError(t, err)
+	return bufimage.ImageToCodeGeneratorRequest(image, param
