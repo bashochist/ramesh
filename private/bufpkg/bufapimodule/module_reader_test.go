d4560c4838bf9dba4701c1ad2f1c9467dd678376
bashochist <121432128+bashochist@users.noreply.github.com>
2021-10-30 11:49:26 +0000

    Add missing whitespace

--- private/bufpkg/bufapimodule/module_reader_test.go
+++ private/bufpkg/bufapimodule/module_reader_test.go
@@ -128,4 +128,144 @@ func testDownload(
 	desc string,
 	tamperProofingEnabled bool,
 	mock *mockDownloadService,
-	erro
+	errorContains string,
+) {
+	t.Helper()
+	t.Run(desc, func(t *testing.T) {
+		t.Parallel()
+		var moduleReaderOpts []ModuleReaderOption
+		if tamperProofingEnabled {
+			moduleReaderOpts = append(moduleReaderOpts, WithTamperProofing())
+		}
+		moduleReader := newModuleReader(mock.factory, moduleReaderOpts...)
+		ctx := context.Background()
+		pin, err := bufmoduleref.NewModulePin(
+			"remote",
+			"owner",
+			"repository",
+			"branch",
+			"commit",
+			"digest",
+			time.Now(),
+		)
+		require.NoError(t, err)
+		module, err := moduleReader.GetModule(ctx, pin)
+		if errorContains != "" {
+			assert.ErrorContains(t, err, errorContains)
+		} else {
+			assert.NotNil(t, module)
+			assert.NoError(t, err)
+		}
+	})
+}
+
+type mockDownloadService struct {
+	module       *modulev1alpha1.Module
+	manifestBlob *modulev1alpha1.Blob
+	blobs        []*modulev1alpha1.Blob
+	err          error
+}
+
+type option interface {
+	apply(*mockDownloadService) error
+}
+
+type filemap map[string][]byte
+
+func (fm filemap) apply(m *mockDownloadService) error {
+	bucket, err := storagemem.NewReadBucket(fm)
+	if err != nil {
+		return err
+	}
+	ctx := context.Background()
+	moduleManifest, blobSet, err := manifest.NewFromBucket(ctx, bucket)
+	if err != nil {
+		return err
+	}
+	mBlob, err := moduleManifest.Blob()
+	if err != nil {
+		return err
+	}
+	m.manifestBlob, err = bufmanifest.AsProtoBlob(ctx, mBlob)
+	if err != nil {
+		return err
+	}
+	blobs := blobSet.Blobs()
+	m.blobs = make([]*modulev1alpha1.Blob, 0, len(blobs))
+	for _, blob := range blobs {
+		protoBlob, err := bufmanifest.AsProtoBlob(ctx, blob)
+		if err != nil {
+			return err
+		}
+		m.blobs = append(m.blobs, protoBlob)
+	}
+	return nil
+}
+
+func withBlobsFromMap(files map[string][]byte) option {
+	return filemap(files)
+}
+
+type retErr struct{ err error }
+
+func (re retErr) apply(m *mockDownloadService) error {
+	m.err = re.err
+	return nil
+}
+
+func withError(err error) option {
+	return retErr{err: err}
+}
+
+type retModule struct{ module *modulev1alpha1.Module }
+
+func (rm retModule) apply(m *mockDownloadService) error {
+	m.module = rm.module
+	return nil
+}
+
+func withModule(module *modulev1alpha1.Module) option {
+	return retModule{module: module}
+}
+
+func newMockDownloadService(
+	t *testing.T,
+	opts ...option,
+) *mockDownloadService {
+	m := &mockDownloadService{}
+	for _, opt := range opts {
+		if err := opt.apply(m); err != nil {
+			t.Error(err)
+		}
+	}
+	return m
+}
+
+func (m *mockDownloadService) factory(_ string) registryv1alpha1connect.DownloadServiceClient {
+	return m
+}
+
+func (m *mockDownloadService) Download(
+	context.Context,
+	*connect.Request[registryv1alpha1.DownloadRequest],
+) (*connect.Response[registryv1alpha1.DownloadResponse], error) {
+	if m.err != nil {
+		return nil, m.err
+	}
+	return connect.NewResponse(&registryv1alpha1.DownloadResponse{
+		Module: m.module,
+	}), nil
+}
+
+func (m *mockDownloadService) DownloadManifestAndBlobs(
+	context.Context,
+	*connect.Request[registryv1alpha1.DownloadManifestAndBlobsRequest],
+) (*connect.Response[registryv1alpha1.DownloadManifestAndBlobsResponse], error) {
+	if m.err != nil {
+		return nil, m.err
+	}
+	return connect.NewResponse(&registryv1alpha1.DownloadManifestAndBlobsResponse{
+		Manifest: m.manifestBlob,
+		Blobs:    m.blobs,
+	}), nil
+}
