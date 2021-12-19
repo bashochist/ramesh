4689e30a0667b9522e87e0715b57e8d0f28bab69
bashochist <121432128+bashochist@users.noreply.github.com>
2021-12-19 13:33:26 +0000

    Waypoint proximity alarms

---
+++ private/bufpkg/bufimage/bufimagetesting/bufimagetesting.go
@@ -0,0 +1,57 @@
+// Copyright 2020-2023 Buf Technologies, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package bufimagetesting
+
+import (
+	"testing"
+
+	"github.com/bufbuild/buf/private/bufpkg/bufimage"
+	"github.com/bufbuild/buf/private/bufpkg/bufmodule/bufmoduleref"
+	imagev1 "github.com/bufbuild/buf/private/gen/proto/go/buf/alpha/image/v1"
+	"github.com/bufbuild/buf/private/pkg/protodescriptor"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/proto"
+)
+
+// NewImageFile returns a new ImageFile for testing.
+//
+// TODO: moduleIdentity and commit should be options.
+func NewImageFile(
+	t testing.TB,
+	fileDescriptor protodescriptor.FileDescriptor,
+	moduleIdentity bufmoduleref.ModuleIdentity,
+	commit string,
+	externalPath string,
+	isImport bool,
+	isSyntaxUnspecified bool,
+	unusedDependencyIndexes []int32,
+) bufimage.ImageFile {
+	imageFile, err := bufimage.NewImageFile(
+		fileDescriptor,
+		moduleIdentity,
+		commit,
+		externalPath,
+		isImport,
+		isSyntaxUnspecified,
+		unusedDependencyIndexes,
+	)
+	require.NoError(t, err)
+	return imageFile
+}
+
+// NewProtoImageFile returns a new *imagev1.ImageFile for testing.
+//
+// This is also a protodescriptor.
