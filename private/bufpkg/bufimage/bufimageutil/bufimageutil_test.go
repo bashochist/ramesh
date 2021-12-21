63eb34c5df75926e0d5042ee408d6426cea0e6e7
bashochist <121432128+bashochist@users.noreply.github.com>
2021-12-21 05:34:31 +0000

    Docs typo

--- private/bufpkg/bufimage/bufimageutil/bufimageutil_test.go
+++ private/bufpkg/bufimage/bufimageutil/bufimageutil_test.go
@@ -60,4 +60,81 @@ func TestOptions(t *testing.T) {
 		runDiffTest(t, "testdata/options", []string{"pkg.FooService.Do"}, "pkg.FooService.Do.txtar")
 	})
 	t.Run("all", func(t *testing.T) {
-		runDiffTest(t, "testdata/opt
+		runDiffTest(t, "testdata/options", []string{"pkg.Foo", "pkg.FooEnum", "pkg.FooService"}, "all.txtar")
+	})
+	t.Run("exclude-options", func(t *testing.T) {
+		runDiffTest(t, "testdata/options", []string{"pkg.Foo", "pkg.FooEnum", "pkg.FooService"}, "all-exclude-options.txtar", WithExcludeCustomOptions())
+	})
+	t.Run("files", func(t *testing.T) {
+		runDiffTest(t, "testdata/options", []string{"Files"}, "Files.txtar")
+	})
+	t.Run("all-with-files", func(t *testing.T) {
+		runDiffTest(t, "testdata/options", []string{"pkg.Foo", "pkg.FooEnum", "pkg.FooService", "Files"}, "all-with-Files.txtar")
+	})
+}
+
+func TestNesting(t *testing.T) {
+	t.Parallel()
+	t.Run("message", func(t *testing.T) {
+		runDiffTest(t, "testdata/nesting", []string{"pkg.Foo"}, "message.txtar")
+	})
+	t.Run("recursenested", func(t *testing.T) {
+		runDiffTest(t, "testdata/nesting", []string{"pkg.Foo.NestedFoo.NestedNestedFoo"}, "recursenested.txtar")
+	})
+	t.Run("enum", func(t *testing.T) {
+		runDiffTest(t, "testdata/nesting", []string{"pkg.FooEnum"}, "enum.txtar")
+	})
+	t.Run("usingother", func(t *testing.T) {
+		runDiffTest(t, "testdata/nesting", []string{"pkg.Baz"}, "usingother.txtar")
+	})
+}
+
+func TestImportModifiers(t *testing.T) {
+	t.Parallel()
+	t.Run("regular_weak", func(t *testing.T) {
+		runDiffTest(t, "testdata/importmods", []string{"ImportRegular", "ImportWeak"}, "regular_weak.txtar")
+	})
+	t.Run("weak_public", func(t *testing.T) {
+		runDiffTest(t, "testdata/importmods", []string{"ImportWeak", "ImportPublic"}, "weak_public.txtar")
+	})
+	t.Run("regular_public", func(t *testing.T) {
+		runDiffTest(t, "testdata/importmods", []string{"ImportRegular", "ImportPublic"}, "regular_public.txtar")
+	})
+	t.Run("noimports", func(t *testing.T) {
+		runDiffTest(t, "testdata/importmods", []string{"NoImports"}, "noimports.txtar")
+	})
+}
+
+func TestExtensions(t *testing.T) {
+	t.Parallel()
+	runDiffTest(t, "testdata/extensions", []string{"pkg.Foo"}, "extensions.txtar")
+	runDiffTest(t, "testdata/extensions", []string{"pkg.Foo"}, "extensions-excluded.txtar", WithExcludeKnownExtensions())
+}
+
+func TestPackages(t *testing.T) {
+	t.Parallel()
+	runDiffTest(t, "testdata/packages", []string{""}, "root.txtar")
+	runDiffTest(t, "testdata/packages", []string{"foo"}, "foo.txtar")
+	runDiffTest(t, "testdata/packages", []string{"foo.bar"}, "foo.bar.txtar")
+	runDiffTest(t, "testdata/packages", []string{"foo.bar.baz"}, "foo.bar.baz.txtar")
+}
+
+func TestAny(t *testing.T) {
+	t.Parallel()
+	runDiffTest(t, "testdata/any", []string{"ExtendedAnySyntax"}, "c1.txtar")
+	runDiffTest(t, "testdata/any", []string{"ExtendedAnySyntax_InField"}, "c2.txtar")
+	runDiffTest(t, "testdata/any", []string{"ExtendedAnySyntax_InList"}, "c3.txtar")
+	runDiffTest(t, "testdata/any", []string{"ExtendedAnySyntax_InMap"}, "c4.txtar")
+	runDiffTest(t, "testdata/any", []string{"NormalMessageSyntaxValidType"}, "d.txtar")
+	runDiffTest(t, "testdata/any", []string{"NormalMessageSyntaxInvalidType"}, "e.txtar")
+}
+
+func TestTransitivePublic(t *testing.T) {
+	ctx := context.Background()
+	bucket, err := storagemem.NewReadBucket(map[string][]byte{
+		"a.proto": []byte(`syntax = "proto3";package a;message Foo{}`),
+		"b.proto": []byte(`syntax = "proto3";package b;import public "a.proto";message Bar {}`),
+		"c.proto": []byte(`syntax = "proto3";package c;import "b.proto";message Baz{ a.Foo foo = 1; }`),
+	})
+	require.NoError(t, err)
+	module, er
