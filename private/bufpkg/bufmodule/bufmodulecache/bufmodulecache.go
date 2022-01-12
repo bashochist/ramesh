8112b8c3aca0369b4dd5aec1b47b6cd8d8451629
bashochist <121432128+bashochist@users.noreply.github.com>
2022-01-12 15:49:58 +0000

    Revert: "net: ip, ipv6: handle gso skbs in forwarding path"

--- private/bufpkg/bufmodule/bufmodulecache/bufmodulecache.go
+++ private/bufpkg/bufmodule/bufmodulecache/bufmodulecache.go
@@ -12,4 +12,79 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package bufmodulecac
+package bufmodulecache
+
+import (
+	"github.com/bufbuild/buf/private/bufpkg/bufmodule"
+	"github.com/bufbuild/buf/private/gen/proto/connect/buf/alpha/registry/v1alpha1/registryv1alpha1connect"
+	"github.com/bufbuild/buf/private/pkg/connectclient"
+	"github.com/bufbuild/buf/private/pkg/filelock"
+	"github.com/bufbuild/buf/private/pkg/storage"
+	"github.com/bufbuild/buf/private/pkg/verbose"
+	"go.uber.org/zap"
+)
+
+// ModuleReaderOption is an option for creating a ModuleReader.
+type ModuleReaderOption func(*moduleReaderOptions)
+
+type RepositoryServiceClientFactory func(address string) registryv1alpha1connect.RepositoryServiceClient
+
+func NewRepositoryServiceClientFactory(clientConfig *connectclient.Config) RepositoryServiceClientFactory {
+	return func(address string) registryv1alpha1connect.RepositoryServiceClient {
+		return connectclient.Make(clientConfig, address, registryv1alpha1connect.NewRepositoryServiceClient)
+	}
+}
+
+// NewModuleReader returns a new ModuleReader that uses cache as a caching layer, and
+// delegate as the source of truth.
+func NewModuleReader(
+	logger *zap.Logger,
+	verbosePrinter verbose.Printer,
+	fileLocker filelock.Locker,
+	dataReadWriteBucket storage.ReadWriteBucket,
+	sumReadWriteBucket storage.ReadWriteBucket,
+	delegate bufmodule.ModuleReader,
+	repositoryClientFactory RepositoryServiceClientFactory,
+	options ...ModuleReaderOption,
+) bufmodule.ModuleReader {
+	return newModuleReader(
+		logger,
+		verbosePrinter,
+		fileLocker,
+		dataReadWriteBucket,
+		sumReadWriteBucket,
+		delegate,
+		repositoryClientFactory,
+		options...,
+	)
+}
+
+// ModuleReaderWithExternalPaths is used to preserve the external paths
+// to the files resolved from the module cache.
+func ModuleReaderWithExternalPaths() ModuleReaderOption {
+	return func(moduleReaderOptions *moduleReaderOptions) {
+		moduleReaderOptions.allowCacheExternalPaths = true
+	}
+}
+
+// NewCASModuleReader creates a new module reader using content addressable storage.
+// This doesn't require file locking and enables support for tamper proofing.
+func NewCASModuleReader(
+	logger *zap.Logger,
+	verbosePrinter verbose.Printer,
+	bucket storage.ReadWriteBucket,
+	delegate bufmodule.ModuleReader,
+	repositoryClientFactory RepositoryServiceClientFactory,
+) bufmodule.ModuleReader {
+	return newCASModuleReader(
+		bucket,
+		delegate,
+		repositoryClientFactory,
+		logger,
+		verbosePrinter,
+	)
+}
+
+type moduleReaderOptions struct {
+	allowCacheExternalPaths bool
+}
