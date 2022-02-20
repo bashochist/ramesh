9311060418fc86ddda2761a21c65d2fdbdd9148b
bashochist <121432128+bashochist@users.noreply.github.com>
2022-02-20 06:44:25 +0000

    adapter

--- private/bufpkg/bufplugin/bufpluginconfig/bufpluginconfig_test.go
+++ private/bufpkg/bufplugin/bufpluginconfig/bufpluginconfig_test.go
@@ -36,4 +36,103 @@ func TestGetConfigForBucket(t *testing.T) {
 	require.NoError(t, err)
 	pluginConfig, err := GetConfigForBucket(context.Background(), readWriteBucket)
 	require.NoError(t, err)
-	pluginIdentity, err := bufpluginref.PluginIdentityForString("buf.bu
+	pluginIdentity, err := bufpluginref.PluginIdentityForString("buf.build/library/go-grpc")
+	require.NoError(t, err)
+	pluginDependency, err := bufpluginref.PluginReferenceForString("buf.build/library/go:v1.28.0", 1)
+	require.NoError(t, err)
+	require.Equal(
+		t,
+		&Config{
+			Name:          pluginIdentity,
+			PluginVersion: "v1.2.0",
+			SourceURL:     "https://github.com/grpc/grpc-go",
+			Description:   "Generates Go language bindings of services in protobuf definition files for gRPC.",
+			Dependencies: []bufpluginref.PluginReference{
+				pluginDependency,
+			},
+			OutputLanguages: []string{"go"},
+			Registry: &RegistryConfig{
+				Go: &GoRegistryConfig{
+					MinVersion: "1.18",
+					Deps: []*GoRegistryDependencyConfig{
+						{
+							Module:  "google.golang.org/grpc",
+							Version: "v1.32.0",
+						},
+					},
+				},
+				Options: map[string]string{
+					"separate_package": "true",
+				},
+			},
+			SPDXLicenseID: "Apache-2.0",
+			LicenseURL:    "https://github.com/grpc/grpc-go/blob/master/LICENSE",
+		},
+		pluginConfig,
+	)
+}
+
+func TestParsePluginConfigGoYAML(t *testing.T) {
+	t.Parallel()
+	pluginConfig, err := ParseConfig(filepath.Join("testdata", "success", "go", "buf.plugin.yaml"))
+	require.NoError(t, err)
+	pluginIdentity, err := bufpluginref.PluginIdentityForString("buf.build/library/go-grpc")
+	require.NoError(t, err)
+	pluginDependency, err := bufpluginref.PluginReferenceForString("buf.build/library/go:v1.28.0", 1)
+	require.NoError(t, err)
+	require.Equal(
+		t,
+		&Config{
+			Name:          pluginIdentity,
+			PluginVersion: "v1.2.0",
+			SourceURL:     "https://github.com/grpc/grpc-go",
+			Description:   "Generates Go language bindings of services in protobuf definition files for gRPC.",
+			Dependencies: []bufpluginref.PluginReference{
+				pluginDependency,
+			},
+			OutputLanguages: []string{"go"},
+			Registry: &RegistryConfig{
+				Go: &GoRegistryConfig{
+					MinVersion: "1.18",
+					Deps: []*GoRegistryDependencyConfig{
+						{
+							Module:  "google.golang.org/grpc",
+							Version: "v1.32.0",
+						},
+					},
+				},
+				Options: map[string]string{
+					"separate_package": "true",
+				},
+			},
+			SPDXLicenseID: "Apache-2.0",
+			LicenseURL:    "https://github.com/grpc/grpc-go/blob/master/LICENSE",
+		},
+		pluginConfig,
+	)
+}
+
+func TestParsePluginConfigGoYAMLOverrideRemote(t *testing.T) {
+	t.Parallel()
+	pluginConfig, err := ParseConfig(filepath.Join("testdata", "success", "go", "buf.plugin.yaml"), WithOverrideRemote("buf.mydomain.com"))
+	require.NoError(t, err)
+	pluginIdentity, err := bufpluginref.PluginIdentityForString("buf.mydomain.com/library/go-grpc")
+	require.NoError(t, err)
+	pluginDependency, err := bufpluginref.PluginReferenceForString("buf.mydomain.com/library/go:v1.28.0", 1)
+	require.NoError(t, err)
+	assert.Equal(t, pluginIdentity, pluginConfig.Name)
+	require.Len(t, pluginConfig.Dependencies, 1)
+	assert.Equal(t, pluginDependency, pluginConfig.Dependencies[0])
+}
+
+func TestParsePluginConfigNPMYAML(t *testing.T) {
+	t.Parallel()
+	pluginConfig, err := ParseConfig(filepath.Join("testdata", "success", "npm", "buf.plugin.yaml"))
+	require.NoError(t, err)
+	pluginIdentity, err := bufpluginref.PluginIdentityForString("buf.build/protocolbuffers/js")
+	require.NoError(t, err)
+	require.Equal(
+		t,
+		&Config{
+			Name:            pluginIdentity,
+			PluginVer
