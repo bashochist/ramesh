9bf3552dcfa4f8b796013a7887d2386f4e4ef8c1
bashochist <121432128+bashochist@users.noreply.github.com>
2022-03-21 16:48:32 +0000

    Create Readme.txt

--- private/bufpkg/buftesting/buftesting.go
+++ private/bufpkg/buftesting/buftesting.go
@@ -41,3 +41,113 @@ const (
 	NumGoogleapisFilesWithImports = 1585
 
 	testGoogleapisCommit = "37c923effe8b002884466074f84bc4e78e6ade62"
+)
+
+var (
+	testHTTPClient = &http.Client{
+		Timeout: 10 * time.Second,
+	}
+	testStorageosProvider = storageos.NewProvider(storageos.ProviderWithSymlinks())
+	testArchiveReader     = githubtesting.NewArchiveReader(
+		zap.NewNop(),
+		testStorageosProvider,
+		testHTTPClient,
+	)
+	testGoogleapisDirPath = filepath.Join("cache", "googleapis")
+)
+
+// GetActualProtocFileDescriptorSet gets the FileDescriptorSet for actual protoc.
+func GetActualProtocFileDescriptorSet(
+	t *testing.T,
+	runner command.Runner,
+	includeImports bool,
+	includeSourceInfo bool,
+	dirPath string,
+	filePaths []string,
+) *descriptorpb.FileDescriptorSet {
+	fileDescriptorSet, err := prototesting.GetProtocFileDescriptorSet(
+		context.Background(),
+		runner,
+		[]string{dirPath},
+		filePaths,
+		includeImports,
+		includeSourceInfo,
+	)
+	require.NoError(t, err)
+	return fileDescriptorSet
+}
+
+// RunActualProtoc runs actual protoc.
+func RunActualProtoc(
+	t *testing.T,
+	runner command.Runner,
+	includeImports bool,
+	includeSourceInfo bool,
+	dirPath string,
+	filePaths []string,
+	env map[string]string,
+	stdout io.Writer,
+	extraFlags ...string,
+) {
+	err := prototesting.RunProtoc(
+		context.Background(),
+		runner,
+		[]string{dirPath},
+		filePaths,
+		includeImports,
+		includeSourceInfo,
+		env,
+		stdout,
+		extraFlags...,
+	)
+	require.NoError(t, err)
+}
+
+// GetGoogleapisDirPath gets the path to a clone of googleapis.
+func GetGoogleapisDirPath(t *testing.T, buftestingDirPath string) string {
+	googleapisDirPath := filepath.Join(buftestingDirPath, testGoogleapisDirPath)
+	require.NoError(
+		t,
+		testArchiveReader.GetArchive(
+			context.Background(),
+			googleapisDirPath,
+			"googleapis",
+			"googleapis",
+			testGoogleapisCommit,
+		),
+	)
+	return googleapisDirPath
+}
+
+// GetProtocFilePaths gets the file paths for protoc.
+//
+// Limit limits the number of files returned if > 0.
+// protoc has a fixed size for number of characters to argument list.
+func GetProtocFilePaths(t *testing.T, dirPath string, limit int) []string {
+	realFilePaths, err := GetProtocFilePathsErr(context.Background(), dirPath, limit)
+	require.NoError(t, err)
+	return realFilePaths
+}
+
+// GetProtocFilePathsErr is like GetProtocFilePaths except it returns an error and accepts a ctx.
+func GetProtocFilePathsErr(ctx context.Context, dirPath string, limit int) ([]string, error) {
+	module, err := bufmodulebuild.NewModuleIncludeBuilder(zap.NewNop(), testStorageosProvider).BuildForIncludes(
+		ctx,
+		[]string{dirPath},
+	)
+	if err != nil {
+		return nil, err
+	}
+	targetFileInfos, err := module.TargetFileInfos(ctx)
+	if err != nil {
+		return nil, err
+	}
+	realFilePaths := make([]string, len(targetFileInfos))
+	for i, fileInfo := range targetFileInfos {
+		realFilePaths[i] = normalpath.Unnormalize(normalpath.Join(dirPath, fileInfo.Path()))
+	}
+	if limit > 0 && len(realFilePaths) > limit {
+		realFilePaths = realFilePaths[:limit]
+	}
+	return realFilePaths, nil
+}
