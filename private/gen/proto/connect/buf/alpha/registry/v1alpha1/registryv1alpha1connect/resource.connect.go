2f6812d15b993ca2460d3e27b93be85b2bd273f1
bashochist <121432128+bashochist@users.noreply.github.com>
2022-04-26 08:04:11 +0000

    Dutch translation

--- private/gen/proto/connect/buf/alpha/registry/v1alpha1/registryv1alpha1connect/resource.connect.go
+++ private/gen/proto/connect/buf/alpha/registry/v1alpha1/registryv1alpha1connect/resource.connect.go
@@ -21,4 +21,68 @@ package registryv1alpha1connect
 import (
 	context "context"
 	errors "errors"
-	v1alpha1 "github.com/bufbuild/buf/private/gen/proto/go/buf/alpha/registry/
+	v1alpha1 "github.com/bufbuild/buf/private/gen/proto/go/buf/alpha/registry/v1alpha1"
+	connect_go "github.com/bufbuild/connect-go"
+	http "net/http"
+	strings "strings"
+)
+
+// This is a compile-time assertion to ensure that this generated file and the connect package are
+// compatible. If you get a compiler error that this constant is not defined, this code was
+// generated with a version of connect newer than the one compiled into your binary. You can fix the
+// problem by either regenerating this code with an older version of connect or updating the connect
+// version compiled into your binary.
+const _ = connect_go.IsAtLeastVersion0_1_0
+
+const (
+	// ResourceServiceName is the fully-qualified name of the ResourceService service.
+	ResourceServiceName = "buf.alpha.registry.v1alpha1.ResourceService"
+)
+
+// ResourceServiceClient is a client for the buf.alpha.registry.v1alpha1.ResourceService service.
+type ResourceServiceClient interface {
+	// GetResourceByName takes a resource name and returns the
+	// resource either as a repository or a plugin.
+	GetResourceByName(context.Context, *connect_go.Request[v1alpha1.GetResourceByNameRequest]) (*connect_go.Response[v1alpha1.GetResourceByNameResponse], error)
+}
+
+// NewResourceServiceClient constructs a client for the buf.alpha.registry.v1alpha1.ResourceService
+// service. By default, it uses the Connect protocol with the binary Protobuf Codec, asks for
+// gzipped responses, and sends uncompressed requests. To use the gRPC or gRPC-Web protocols, supply
+// the connect.WithGRPC() or connect.WithGRPCWeb() options.
+//
+// The URL supplied here should be the base URL for the Connect or gRPC server (for example,
+// http://api.acme.com or https://acme.com/grpc).
+func NewResourceServiceClient(httpClient connect_go.HTTPClient, baseURL string, opts ...connect_go.ClientOption) ResourceServiceClient {
+	baseURL = strings.TrimRight(baseURL, "/")
+	return &resourceServiceClient{
+		getResourceByName: connect_go.NewClient[v1alpha1.GetResourceByNameRequest, v1alpha1.GetResourceByNameResponse](
+			httpClient,
+			baseURL+"/buf.alpha.registry.v1alpha1.ResourceService/GetResourceByName",
+			opts...,
+		),
+	}
+}
+
+// resourceServiceClient implements ResourceServiceClient.
+type resourceServiceClient struct {
+	getResourceByName *connect_go.Client[v1alpha1.GetResourceByNameRequest, v1alpha1.GetResourceByNameResponse]
+}
+
+// GetResourceByName calls buf.alpha.registry.v1alpha1.ResourceService.GetResourceByName.
+func (c *resourceServiceClient) GetResourceByName(ctx context.Context, req *connect_go.Request[v1alpha1.GetResourceByNameRequest]) (*connect_go.Response[v1alpha1.GetResourceByNameResponse], error) {
+	return c.getResourceByName.CallUnary(ctx, req)
+}
+
+// ResourceServiceHandler is an implementation of the buf.alpha.registry.v1alpha1.ResourceService
+// service.
+type ResourceServiceHandler interface {
+	// GetResourceByName takes a resource name and returns the
+	// resource either as a repository or a plugin.
+	GetResourceByName(context.Context, *connect_go.Request[v1alpha1.GetResourceByNameRequest]) (*connect_go.Response[v1alpha1.GetResourceByNameResponse], error)
+}
+
+// NewResourceServiceHandler builds an HTTP handler from the service implementation. It returns the
+// path on which to mount the handler and the handler itself.
+//
+// By default, handlers support the Connect,
