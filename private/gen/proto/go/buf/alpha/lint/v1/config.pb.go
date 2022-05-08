870abf39b301e6a92a01e000f30727e1247f2373
bashochist <121432128+bashochist@users.noreply.github.com>
2022-05-08 08:46:09 +0000

    Reverted previous commit

--- private/gen/proto/go/buf/alpha/lint/v1/config.pb.go
+++ private/gen/proto/go/buf/alpha/lint/v1/config.pb.go
@@ -296,4 +296,85 @@ var file_buf_alpha_lint_v1_config_proto_rawDesc = []byte{
 	0x03, 0x42, 0x41, 0x4c, 0xaa, 0x02, 0x11, 0x42, 0x75, 0x66, 0x2e, 0x41, 0x6c, 0x70, 0x68, 0x61,
 	0x2e, 0x4c, 0x69, 0x6e, 0x74, 0x2e, 0x56, 0x31, 0xca, 0x02, 0x11, 0x42, 0x75, 0x66, 0x5c, 0x41,
 	0x6c, 0x70, 0x68, 0x61, 0x5c, 0x4c, 0x69, 0x6e, 0x74, 0x5c, 0x56, 0x31, 0xe2, 0x02, 0x1d, 0x42,
-	0x75, 0x66, 0x5c, 0x41, 0x6c, 0x70, 0x68, 0x61, 0x5c, 0x4c, 0x69, 0x6e, 0x
+	0x75, 0x66, 0x5c, 0x41, 0x6c, 0x70, 0x68, 0x61, 0x5c, 0x4c, 0x69, 0x6e, 0x74, 0x5c, 0x56, 0x31,
+	0x5c, 0x47, 0x50, 0x42, 0x4d, 0x65, 0x74, 0x61, 0x64, 0x61, 0x74, 0x61, 0xea, 0x02, 0x14, 0x42,
+	0x75, 0x66, 0x3a, 0x3a, 0x41, 0x6c, 0x70, 0x68, 0x61, 0x3a, 0x3a, 0x4c, 0x69, 0x6e, 0x74, 0x3a,
+	0x3a, 0x56, 0x31, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+}
+
+var (
+	file_buf_alpha_lint_v1_config_proto_rawDescOnce sync.Once
+	file_buf_alpha_lint_v1_config_proto_rawDescData = file_buf_alpha_lint_v1_config_proto_rawDesc
+)
+
+func file_buf_alpha_lint_v1_config_proto_rawDescGZIP() []byte {
+	file_buf_alpha_lint_v1_config_proto_rawDescOnce.Do(func() {
+		file_buf_alpha_lint_v1_config_proto_rawDescData = protoimpl.X.CompressGZIP(file_buf_alpha_lint_v1_config_proto_rawDescData)
+	})
+	return file_buf_alpha_lint_v1_config_proto_rawDescData
+}
+
+var file_buf_alpha_lint_v1_config_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_buf_alpha_lint_v1_config_proto_goTypes = []interface{}{
+	(*Config)(nil),  // 0: buf.alpha.lint.v1.Config
+	(*IDPaths)(nil), // 1: buf.alpha.lint.v1.IDPaths
+}
+var file_buf_alpha_lint_v1_config_proto_depIdxs = []int32{
+	1, // 0: buf.alpha.lint.v1.Config.ignore_id_paths:type_name -> buf.alpha.lint.v1.IDPaths
+	1, // [1:1] is the sub-list for method output_type
+	1, // [1:1] is the sub-list for method input_type
+	1, // [1:1] is the sub-list for extension type_name
+	1, // [1:1] is the sub-list for extension extendee
+	0, // [0:1] is the sub-list for field type_name
+}
+
+func init() { file_buf_alpha_lint_v1_config_proto_init() }
+func file_buf_alpha_lint_v1_config_proto_init() {
+	if File_buf_alpha_lint_v1_config_proto != nil {
+		return
+	}
+	if !protoimpl.UnsafeEnabled {
+		file_buf_alpha_lint_v1_config_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*Config); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_buf_alpha_lint_v1_config_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*IDPaths); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+	}
+	type x struct{}
+	out := protoimpl.TypeBuilder{
+		File: protoimpl.DescBuilder{
+			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
+			RawDescriptor: file_buf_alpha_lint_v1_config_proto_rawDesc,
+			NumEnums:      0,
+			NumMessages:   2,
+			NumExtensions: 0,
+			NumServices:   0,
+		},
+		GoTypes:           file_buf_alpha_lint_v1_config_proto_goTypes,
+		DependencyIndexes: file_buf_alpha_lint_v1_config_proto_depIdxs,
+		MessageInfos:      file_buf_alpha_lint_v1_config_proto_msgTypes,
+	}.Build()
+	File_buf_alpha_lint_v1_config_proto = out.File
+	file_buf_alpha_lint_v1_config_proto_rawDesc = nil
+	file_buf_alpha_lint_v1_config_proto_goTypes = nil
+	file_buf_alpha_lint_v1_config_proto_depIdxs = nil
+}
