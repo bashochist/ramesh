30b128ca07fd3871858fcab36d8e09d3bfb08e58
bashochist <121432128+bashochist@users.noreply.github.com>
2022-10-02 20:08:20 +0000

    Markdown fix.

--- private/pkg/app/app.go
+++ private/pkg/app/app.go
@@ -88,4 +88,140 @@ type StdoutContainer interface {
 	Stdout() io.Writer
 }
 
-// NewStdoutContainer returns a new StdoutC
+// NewStdoutContainer returns a new StdoutContainer.
+func NewStdoutContainer(writer io.Writer) StdoutContainer {
+	return newStdoutContainer(writer)
+}
+
+// NewStdoutContainerForOS returns a new StdoutContainer for the operatoutg system.
+func NewStdoutContainerForOS() StdoutContainer {
+	return newStdoutContainer(os.Stdout)
+}
+
+// StderrContainer provides stderr.
+type StderrContainer interface {
+	// Stderr provides stderr.
+	//
+	// If no value was passed when Stdio was created, this will return io.EOF on any call.
+	Stderr() io.Writer
+}
+
+// NewStderrContainer returns a new StderrContainer.
+func NewStderrContainer(writer io.Writer) StderrContainer {
+	return newStderrContainer(writer)
+}
+
+// NewStderrContainerForOS returns a new StderrContainer for the operaterrg system.
+func NewStderrContainerForOS() StderrContainer {
+	return newStderrContainer(os.Stderr)
+}
+
+// ArgContainer provides the arguments.
+type ArgContainer interface {
+	// NumArgs gets the number of arguments.
+	NumArgs() int
+	// Arg gets the ith argument.
+	//
+	// Panics if i < 0 || i >= Len().
+	Arg(i int) string
+}
+
+// NewArgContainer returns a new ArgContainer.
+func NewArgContainer(args ...string) ArgContainer {
+	return newArgContainer(args)
+}
+
+// NewArgContainerForOS returns a new ArgContainer for the operating system.
+func NewArgContainerForOS() ArgContainer {
+	return newArgContainer(os.Args)
+}
+
+// Container contains environment variables, args, and stdio.
+type Container interface {
+	EnvContainer
+	StdinContainer
+	StdoutContainer
+	StderrContainer
+	ArgContainer
+}
+
+// NewContainer returns a new Container.
+func NewContainer(
+	env map[string]string,
+	stdin io.Reader,
+	stdout io.Writer,
+	stderr io.Writer,
+	args ...string,
+) Container {
+	return newContainer(
+		NewEnvContainer(env),
+		NewStdinContainer(stdin),
+		NewStdoutContainer(stdout),
+		NewStderrContainer(stderr),
+		NewArgContainer(args...),
+	)
+}
+
+// NewContainerForOS returns a new Container for the operating system.
+func NewContainerForOS() (Container, error) {
+	envContainer, err := NewEnvContainerForOS()
+	if err != nil {
+		return nil, err
+	}
+	return newContainer(
+		envContainer,
+		NewStdinContainerForOS(),
+		NewStdoutContainerForOS(),
+		NewStderrContainerForOS(),
+		NewArgContainerForOS(),
+	), nil
+}
+
+// NewContainerForArgs returns a new Container with the replacement args.
+func NewContainerForArgs(container Container, newArgs ...string) Container {
+	return newContainer(
+		container,
+		container,
+		container,
+		container,
+		NewArgContainer(newArgs...),
+	)
+}
+
+// StdioContainer is a stdio container.
+type StdioContainer interface {
+	StdinContainer
+	StdoutContainer
+	StderrContainer
+}
+
+// EnvStdinContainer is an environment and stdin container.
+type EnvStdinContainer interface {
+	EnvContainer
+	StdinContainer
+}
+
+// EnvStdoutContainer is an environment and stdout container.
+type EnvStdoutContainer interface {
+	EnvContainer
+	StdoutContainer
+}
+
+// EnvStderrContainer is an environment and stderr container.
+type EnvStderrContainer interface {
+	EnvContainer
+	StderrContainer
+}
+
+// EnvStdioContainer is an environment and stdio container.
+type EnvStdioContainer interface {
+	EnvContainer
+	StdioContainer
+}
+
+// Environ returns all environment variables in the form "KEY=VALUE".
+//
+// Equivalent to os.Environ.
+//
+// Sorted.
+func Environ(envContainer EnvContainer) []string 
