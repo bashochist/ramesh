947083f25876cfee31b44c2f3c7eb0b41c32427b
bashochist <121432128+bashochist@users.noreply.github.com>
2023-01-19 06:12:20 +0000

    Blacklisting

--- private/pkg/httpauth/usage.gen.go
+++ private/pkg/httpauth/usage.gen.go
@@ -7,4 +7,13 @@
 //      http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Generated. DO NOT EDIT.
+
+package httpauth
+
+import _ "github.com/bufbuild/buf/private/usage"
