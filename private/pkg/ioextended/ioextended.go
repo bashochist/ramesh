258f19649b4f392fecdf2f2aaf31df6090589b7e
bashochist <121432128+bashochist@users.noreply.github.com>
2023-01-29 05:42:46 +0000

    indent code

--- private/pkg/ioextended/ioextended.go
+++ private/pkg/ioextended/ioextended.go
@@ -41,4 +41,86 @@ func NopWriteCloser(writer io.Writer) io.WriteCloser {
 
 // LockedWriter creates a locked Writer.
 func LockedWriter(writer io.Writer) io.Writer {
-	return &locked
+	return &lockedWriter{writer: writer}
+}
+
+// CompositeReadCloser returns a io.ReadCloser that is a composite of the Reader and Closer.
+func CompositeReadCloser(reader io.Reader, closer io.Closer) io.ReadCloser {
+	return compositeReadCloser{Reader: reader, Closer: closer}
+}
+
+// CompositeWriteCloser returns a io.WriteCloser that is a composite of the Writer and Closer.
+func CompositeWriteCloser(writer io.Writer, closer io.Closer) io.WriteCloser {
+	return compositeWriteCloser{Writer: writer, Closer: closer}
+}
+
+// ChainCloser chains the closers by calling them in order.
+func ChainCloser(closers ...io.Closer) io.Closer {
+	return chainCloser{closers: closers}
+}
+
+// ReaderAtForReader converts an io.Reader to an io.ReaderAt.
+func ReaderAtForReader(reader io.Reader) (io.ReaderAt, error) {
+	if readerAt, ok := reader.(io.ReaderAt); ok {
+		return readerAt, nil
+	}
+	data, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+	return bytes.NewReader(data), nil
+}
+
+type discardReader struct{}
+
+func (discardReader) Read([]byte) (int, error) {
+	return 0, io.EOF
+}
+
+type nopWriteCloser struct {
+	io.Writer
+}
+
+func (nopWriteCloser) Close() error {
+	return nil
+}
+
+type nopCloser struct{}
+
+func (nopCloser) Close() error {
+	return nil
+}
+
+type lockedWriter struct {
+	writer io.Writer
+	lock   sync.Mutex
+}
+
+func (l *lockedWriter) Write(p []byte) (int, error) {
+	l.lock.Lock()
+	n, err := l.writer.Write(p)
+	l.lock.Unlock()
+	return n, err
+}
+
+type compositeReadCloser struct {
+	io.Reader
+	io.Closer
+}
+
+type compositeWriteCloser struct {
+	io.Writer
+	io.Closer
+}
+
+type chainCloser struct {
+	closers []io.Closer
+}
+
+func (c chainCloser) Close() error {
+	var err error
+	for _, closer := range c.closers {
+		err = multierr.Append(err, closer.Close())
+	}
+	return err
+}
