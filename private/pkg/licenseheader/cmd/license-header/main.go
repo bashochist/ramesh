2fa71280917ca7ab14aea678e9b8f3962ace35af
bashochist <121432128+bashochist@users.noreply.github.com>
2023-01-29 05:50:04 +0000

    sh

--- private/pkg/licenseheader/cmd/license-header/main.go
+++ private/pkg/licenseheader/cmd/license-header/main.go
@@ -22,4 +22,134 @@ import (
 	"regexp"
 
 	"github.com/bufbuild/buf/private/pkg/app"
-	"github.com/bufbuild/buf/private/pkg/app
+	"github.com/bufbuild/buf/private/pkg/app/appcmd"
+	"github.com/bufbuild/buf/private/pkg/command"
+	"github.com/bufbuild/buf/private/pkg/diff"
+	"github.com/bufbuild/buf/private/pkg/git"
+	"github.com/bufbuild/buf/private/pkg/licenseheader"
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+)
+
+const (
+	use = "license-header"
+
+	copyrightHolderFlagName = "copyright-holder"
+	licenseTypeFlagName     = "license-type"
+	yearRangeFlagName       = "year-range"
+	diffFlagName            = "diff"
+	exitCodeFlagName        = "exit-code"
+	ignoreFlagName          = "ignore"
+	ignoreFlagShortName     = "e"
+)
+
+func main() {
+	appcmd.Main(context.Background(), newCommand())
+}
+
+func newCommand() *appcmd.Command {
+	flags := newFlags()
+	return &appcmd.Command{
+		Use: use + " files...",
+		Run: func(ctx context.Context, container app.Container) error {
+			return run(ctx, container, flags)
+		},
+		BindFlags: flags.Bind,
+	}
+}
+
+type flags struct {
+	LicenseType     string
+	CopyrightHolder string
+	YearRange       string
+	Diff            bool
+	ExitCode        bool
+	Ignore          []string
+}
+
+func newFlags() *flags {
+	return &flags{}
+}
+
+func (f *flags) Bind(flagSet *pflag.FlagSet) {
+	flagSet.StringVar(
+		&f.LicenseType,
+		licenseTypeFlagName,
+		"",
+		"The license type. Must be one of [none,apache,proprietary].",
+	)
+	_ = cobra.MarkFlagRequired(flagSet, licenseTypeFlagName)
+	flagSet.StringVar(
+		&f.CopyrightHolder,
+		copyrightHolderFlagName,
+		"",
+		"The copyright holder. Required if license type is not none.",
+	)
+	flagSet.StringVar(
+		&f.YearRange,
+		yearRangeFlagName,
+		"",
+		"The year range. Required if license type is not none.",
+	)
+	flagSet.BoolVar(
+		&f.Diff,
+		diffFlagName,
+		false,
+		"Print a diff instead of modifying the files.",
+	)
+	flagSet.BoolVar(
+		&f.ExitCode,
+		exitCodeFlagName,
+		false,
+		fmt.Sprintf("Exit with a non-zero exit code if a diff is present. Only valid with %s.", diffFlagName),
+	)
+	flagSet.StringSliceVarP(
+		&f.Ignore,
+		ignoreFlagName,
+		ignoreFlagShortName,
+		nil,
+		`File paths to ignore.
+These are extended regexes in the style of egrep.
+If a file matches any of these values, it will be ignored.
+Only works if there are no arguments and license-header does its own search for files.`,
+	)
+}
+
+func run(ctx context.Context, container app.Container, flags *flags) error {
+	if flags.ExitCode && !flags.Diff {
+		return appcmd.NewInvalidArgumentErrorf("cannot specify %s without %s", exitCodeFlagName, diffFlagName)
+	}
+	licenseType, err := licenseheader.ParseLicenseType(flags.LicenseType)
+	if err != nil {
+		return appcmd.NewInvalidArgumentErrorf("--%s: %v", licenseTypeFlagName, err)
+	}
+	if licenseType != licenseheader.LicenseTypeNone {
+		if flags.CopyrightHolder == "" {
+			return newRequiredFlagError(copyrightHolderFlagName)
+		}
+		if flags.YearRange == "" {
+			return newRequiredFlagError(yearRangeFlagName)
+		}
+	}
+	runner := command.NewRunner()
+	filenames, err := getFilenames(ctx, container, runner, flags.Ignore)
+	if err != nil {
+		return err
+	}
+	for _, filename := range filenames {
+		data, err := os.ReadFile(filename)
+		if err != nil {
+			return err
+		}
+		modifiedData, err := licenseheader.Modify(
+			licenseType,
+			flags.CopyrightHolder,
+			flags.YearRange,
+			filename,
+			data,
+		)
+		if err != nil {
+			return err
+		}
+		if !bytes.Equal(data, modifiedData) {
+		
