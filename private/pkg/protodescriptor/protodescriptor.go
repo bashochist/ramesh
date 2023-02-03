abddb2d5ff2efc8fd5a9e611fba0c69cb0fe5c82
bashochist <121432128+bashochist@users.noreply.github.com>
2023-02-03 16:23:35 +0000

    Layout improvements

--- private/pkg/protodescriptor/protodescriptor.go
+++ private/pkg/protodescriptor/protodescriptor.go
@@ -143,4 +143,104 @@ func ValidateFileDescriptor(fileDescriptor FileDescriptor) error {
 }
 
 // ValidateCodeGeneratorRequest validates the CodeGeneratorRequest.
-func ValidateCodeGeneratorRequest(request *pluginp
+func ValidateCodeGeneratorRequest(request *pluginpb.CodeGeneratorRequest) error {
+	if err := ValidateCodeGeneratorRequestExceptFileDescriptorProtos(request); err != nil {
+		return err
+	}
+	for _, fileDescriptorProto := range request.ProtoFile {
+		if err := ValidateFileDescriptor(fileDescriptorProto); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// ValidateCodeGeneratorRequestExceptFileDescriptorProtos validates the CodeGeneratorRequest
+// minus the FileDescriptorProtos.
+func ValidateCodeGeneratorRequestExceptFileDescriptorProtos(request *pluginpb.CodeGeneratorRequest) error {
+	if request == nil {
+		return errors.New("nil CodeGeneratorRequest")
+	}
+	if len(request.ProtoFile) == 0 {
+		return errors.New("empty CodeGeneratorRequest.ProtoFile")
+	}
+	if len(request.FileToGenerate) == 0 {
+		return errors.New("empty CodeGeneratorRequest.FileToGenerate")
+	}
+	if err := ValidateProtoPaths("CodeGeneratorRequest.FileToGenerate", request.FileToGenerate); err != nil {
+		return err
+	}
+	return nil
+}
+
+// ValidateCodeGeneratorResponse validates the CodeGeneratorResponse.
+//
+// This validates that names are set.
+//
+// It is actually OK per the plugin.proto specs to not have the name set, and
+// if this is empty, the content should be combined with the previous file.
+// However, for our handlers, we do not support this, and for our
+// binary handlers, we combine CodeGeneratorResponse.File contents.
+//
+// https://github.com/protocolbuffers/protobuf/blob/b99994d994e399174fe688a5efbcb6d91f36952a/src/google/protobuf/compiler/plugin.proto#L127
+func ValidateCodeGeneratorResponse(response *pluginpb.CodeGeneratorResponse) error {
+	if response == nil {
+		return errors.New("nil CodeGeneratorResponse")
+	}
+	for _, file := range response.File {
+		if file.GetName() == "" {
+			return errors.New("empty CodeGeneratorResponse.File.Name")
+		}
+	}
+	return nil
+}
+
+// ValidateProtoPath validates the proto path.
+//
+// This checks that the path is normalized and ends in .proto.
+func ValidateProtoPath(name string, path string) error {
+	if path == "" {
+		return fmt.Errorf("%s is empty", name)
+	}
+	normalized, err := normalpath.NormalizeAndValidate(path)
+	if err != nil {
+		return fmt.Errorf("%s had normalization error: %w", name, err)
+	}
+	if path != normalized {
+		return fmt.Errorf("%s %s was not normalized to %s", name, path, normalized)
+	}
+	if normalpath.Ext(path) != ".proto" {
+		return fmt.Errorf("%s %s does not have a .proto extension", name, path)
+	}
+	return nil
+}
+
+// ValidateProtoPaths validates the proto paths.
+//
+// This checks that the paths are normalized and end in .proto.
+func ValidateProtoPaths(name string, paths []string) error {
+	for _, path := range paths {
+		if err := ValidateProtoPath(name, path); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// FieldDescriptorProtoTypePrettyString prints a pretty string
+// representation of the FieldDescriptorProto_Type.
+func FieldDescriptorProtoTypePrettyString(t descriptorpb.FieldDescriptorProto_Type) string {
+	switch t {
+	case descriptorpb.FieldDescriptorProto_TYPE_DOUBLE:
+		return "double"
+	case descriptorpb.FieldDescriptorProto_TYPE_FLOAT:
+		return "float"
+	case descriptorpb.FieldDescriptorProto_TYPE_INT64:
+		return "int64"
+	case descriptorpb.FieldDescriptorProto_TYPE_UINT64:
+		return "uint64"
+	case descriptorpb.FieldDescriptorProto_TYPE_INT32:
+		return "int32"
+	case descriptorpb.FieldDescriptorProto_TYPE_FIXED64:
+		return "fixed64"
+	case descri
