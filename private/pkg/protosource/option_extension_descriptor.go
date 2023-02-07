e844b533f0f0fbec63c916f9ca9835f5251be1bf
bashochist <121432128+bashochist@users.noreply.github.com>
2023-02-07 19:01:56 +0000

    updated the website

--- private/pkg/protosource/option_extension_descriptor.go
+++ private/pkg/protosource/option_extension_descriptor.go
@@ -24,4 +24,51 @@ type optionExtensionDescriptor struct {
 	message proto.Message
 }
 
-func newOptio
+func newOptionExtensionDescriptor(message proto.Message) optionExtensionDescriptor {
+	return optionExtensionDescriptor{
+		message: message,
+	}
+}
+
+func (o *optionExtensionDescriptor) OptionExtension(extensionType protoreflect.ExtensionType) (interface{}, bool) {
+	if extensionType.TypeDescriptor().ContainingMessage().FullName() != o.message.ProtoReflect().Descriptor().FullName() {
+		return nil, false
+	}
+	if !proto.HasExtension(o.message, extensionType) {
+		return nil, false
+	}
+	return proto.GetExtension(o.message, extensionType), true
+}
+
+func (o *optionExtensionDescriptor) PresentExtensionNumbers() []int32 {
+	fieldNumbersSet := map[int32]struct{}{}
+	var fieldNumbers []int32
+	addFieldNumber := func(fieldNo int32) {
+		if _, ok := fieldNumbersSet[fieldNo]; !ok {
+			fieldNumbersSet[fieldNo] = struct{}{}
+			fieldNumbers = append(fieldNumbers, fieldNo)
+		}
+	}
+	msg := o.message.ProtoReflect()
+	extensionRanges := msg.Descriptor().ExtensionRanges()
+	for b := msg.GetUnknown(); len(b) > 0; {
+		fieldNo, _, n := protowire.ConsumeField(b)
+		if extensionRanges.Has(fieldNo) {
+			addFieldNumber(int32(fieldNo))
+		}
+		b = b[n:]
+	}
+	// Extensions for google.protobuf.*Options are a bit of a special case
+	// as the extensions in a FileDescriptorSet message may differ with
+	// the extensions defined in the proto with which buf is compiled.
+	//
+	// Also loop through known extensions here to get extension numbers.
+	msg.Range(func(fieldDescriptor protoreflect.FieldDescriptor, _ protoreflect.Value) bool {
+		if fieldDescriptor.IsExtension() {
+			addFieldNumber(int32(fieldDescriptor.Number()))
+		}
+		return true
+	})
+
+	return fieldNumbers
+}
