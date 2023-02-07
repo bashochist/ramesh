56b0175543eb7b30550ba183a7aabc58cbd634a6
bashochist <121432128+bashochist@users.noreply.github.com>
2023-02-07 06:19:35 +0000

    chore(package): update eslint to version 4.7.0

--- private/pkg/protosource/location_store.go
+++ private/pkg/protosource/location_store.go
@@ -51,4 +51,54 @@ func (l *locationStore) getLocationByPathKey(pathKey string) Location {
 	}
 
 	// build index and get sourceCodeInfoLocation
-	l.sourceCodeInfoLocationLo
+	l.sourceCodeInfoLocationLock.RLock()
+	pathToSourceCodeInfoLocation := l.pathToSourceCodeInfoLocation
+	l.sourceCodeInfoLocationLock.RUnlock()
+	if pathToSourceCodeInfoLocation == nil {
+		l.sourceCodeInfoLocationLock.Lock()
+		pathToSourceCodeInfoLocation = l.pathToSourceCodeInfoLocation
+		if pathToSourceCodeInfoLocation == nil {
+			pathToSourceCodeInfoLocation = make(map[string]*descriptorpb.SourceCodeInfo_Location)
+			for _, sourceCodeInfoLocation := range l.sourceCodeInfoLocations {
+				pathKey := getPathKey(sourceCodeInfoLocation.Path)
+				// - Multiple locations may have the same path.  This happens when a single
+				//   logical declaration is spread out across multiple places.  The most
+				//   obvious example is the "extend" block again -- there may be multiple
+				//   extend blocks in the same scope, each of which will have the same path.
+				if _, ok := pathToSourceCodeInfoLocation[pathKey]; !ok {
+					pathToSourceCodeInfoLocation[pathKey] = sourceCodeInfoLocation
+				}
+			}
+		}
+		l.pathToSourceCodeInfoLocation = pathToSourceCodeInfoLocation
+		l.sourceCodeInfoLocationLock.Unlock()
+	}
+	sourceCodeInfoLocation, ok := pathToSourceCodeInfoLocation[pathKey]
+	if !ok {
+		return nil
+	}
+
+	// populate cache and return
+	if sourceCodeInfoLocation == nil {
+		location = nil
+	} else {
+		location = newLocation(sourceCodeInfoLocation)
+	}
+	l.locationLock.Lock()
+	l.pathToLocation[pathKey] = location
+	l.locationLock.Unlock()
+	return location
+}
+
+func getPathKey(path []int32) string {
+	key := make([]byte, len(path)*4)
+	j := 0
+	for _, elem := range path {
+		key[j] = byte(elem)
+		key[j+1] = byte(elem >> 8)
+		key[j+2] = byte(elem >> 16)
+		key[j+3] = byte(elem >> 24)
+		j += 4
+	}
+	return string(key)
+}
