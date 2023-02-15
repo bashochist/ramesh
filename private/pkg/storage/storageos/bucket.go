0f5ff66003ac11cd58132e41d323711f787fec5e
bashochist <121432128+bashochist@users.noreply.github.com>
2023-02-15 16:46:24 +0000

    Update predatory.txt

--- private/pkg/storage/storageos/bucket.go
+++ private/pkg/storage/storageos/bucket.go
@@ -21,4 +21,122 @@ import (
 	"path/filepath"
 	"strings"
 
-	"gith
+	"github.com/bufbuild/buf/private/pkg/filepathextended"
+	"github.com/bufbuild/buf/private/pkg/normalpath"
+	"github.com/bufbuild/buf/private/pkg/storage"
+	"github.com/bufbuild/buf/private/pkg/storage/storageutil"
+	"go.uber.org/atomic"
+	"go.uber.org/multierr"
+)
+
+// errNotDir is the error returned if a path is not a directory.
+var errNotDir = errors.New("not a directory")
+
+type bucket struct {
+	rootPath         string
+	absoluteRootPath string
+	symlinks         bool
+}
+
+func newBucket(rootPath string, symlinks bool) (*bucket, error) {
+	rootPath = normalpath.Unnormalize(rootPath)
+	if err := validateDirPathExists(rootPath, symlinks); err != nil {
+		return nil, err
+	}
+	absoluteRootPath, err := filepath.Abs(rootPath)
+	if err != nil {
+		return nil, err
+	}
+	// do not validate - allow anything with OS buckets including
+	// absolute paths and jumping context
+	rootPath = normalpath.Normalize(rootPath)
+	return &bucket{
+		rootPath:         rootPath,
+		absoluteRootPath: absoluteRootPath,
+		symlinks:         symlinks,
+	}, nil
+}
+
+func (b *bucket) Get(ctx context.Context, path string) (storage.ReadObjectCloser, error) {
+	externalPath, err := b.getExternalPath(path)
+	if err != nil {
+		return nil, err
+	}
+	if err := b.validateExternalPath(path, externalPath); err != nil {
+		return nil, err
+	}
+	resolvedPath := externalPath
+	if b.symlinks {
+		resolvedPath, err = filepath.EvalSymlinks(externalPath)
+		if err != nil {
+			return nil, err
+		}
+	}
+	file, err := os.Open(resolvedPath)
+	if err != nil {
+		return nil, err
+	}
+	// we could use fileInfo.Name() however we might as well use the externalPath
+	return newReadObjectCloser(
+		path,
+		externalPath,
+		file,
+	), nil
+}
+
+func (b *bucket) Stat(ctx context.Context, path string) (storage.ObjectInfo, error) {
+	externalPath, err := b.getExternalPath(path)
+	if err != nil {
+		return nil, err
+	}
+	if err := b.validateExternalPath(path, externalPath); err != nil {
+		return nil, err
+	}
+	// we could use fileInfo.Name() however we might as well use the externalPath
+	return storageutil.NewObjectInfo(
+		path,
+		externalPath,
+	), nil
+}
+
+func (b *bucket) Walk(
+	ctx context.Context,
+	prefix string,
+	f func(storage.ObjectInfo) error,
+) error {
+	externalPrefix, err := b.getExternalPrefix(prefix)
+	if err != nil {
+		return err
+	}
+	walkChecker := storageutil.NewWalkChecker()
+	var walkOptions []filepathextended.WalkOption
+	if b.symlinks {
+		walkOptions = append(walkOptions, filepathextended.WalkWithSymlinks())
+	}
+	if err := filepathextended.Walk(
+		externalPrefix,
+		func(externalPath string, fileInfo os.FileInfo, err error) error {
+			if err != nil {
+				// this can happen if a symlink is broken
+				// in this case, we just want to continue the walk
+				if b.symlinks && os.IsNotExist(err) {
+					return nil
+				}
+				return err
+			}
+			if err := walkChecker.Check(ctx); err != nil {
+				return err
+			}
+			absoluteExternalPath, err := filepath.Abs(externalPath)
+			if err != nil {
+				return err
+			}
+			if fileInfo.Mode().IsRegular() {
+				path, err := normalpath.Rel(b.absoluteRootPath, absoluteExternalPath)
+				if err != nil {
+					return err
+				}
+				// just in case
+				path, err = normalpath.NormalizeAndValidate(path)
+				if err != nil {
+					retur
